9d7fbd029ad487d2be970126bdb14b71e83d5e57
Stravinsky Anglade <stravinsky.anglade@mykenzie.snhu.edu>
2023-07-31 10:33:01 -0400

    modify csst

---
+++ src/Thankyou.js
@@ -0,0 +1,12 @@
+import Thanks from './components/thanks';
+import './App.css';
+
+function Thankyou() {
+  return (
+    <div className="align-center">
+     <Thanks />
+    </div>
+  );
+}
+
+export default Thankyou;
---
+++ src/components/thanks.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function thanks() {
+  return (
+    <div>
+        <p>
+            Thank you for your submission
+        </p>
+        <p>
+            I will contact you after review
+        </p>
+    </div>
+  )
+}
+
+export default thanks
